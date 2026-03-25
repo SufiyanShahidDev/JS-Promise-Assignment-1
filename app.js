abde619f8f48e425616eb08a005e96a9a8ed4eef
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-03-25 21:12:05 +0500

    Initial commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const delayValue = () => {
     return new Promise = (resolve, reject) => {
         if(typeof ms !== "number" || ms < 0){
             return reject("Invalid delay time")
+        }else{
+            
         }
     }
 } 
